f63192eb2b291fcf4093c2980b5d70e07ffb8f23
meteor4o <meteor@bitex.bg>
2022-04-16 11:11:11 +0300

    creation

---
+++ src/main/java/com/example/coursera_project/CourseraProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursera_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseraProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseraProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coursera_project/CourseraProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursera_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseraProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
